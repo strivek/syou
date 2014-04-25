f043652f60471533f386c67f2991a1697a2d18ed
nailu0 <strivek@163.com>
2014-04-25 17:46:28 +0800

    扩展jquery多级下拉框特效

--- js/listdownback.js
+++ js/listdownback.js
@@ -4,25 +4,30 @@
 $((function () {
     $.fn.listDownBack = function (options) {
         //默认时间
-        var settings = $.extend({time: 1}, options);
+        var settings = $.extend({time: 0.6}, options);
         return $.each(this, function (options) {
             var $list = $(this).find(" > ul"),
-                $link = $(this).find(":first-child");
-            listHeight = $list.height(),
-                hideElem = {height: 0, overflow: 'hidden'}
+                $link = $(this).find("> :first-child"),
+                hideElem = {overflow: 'hidden', display: 'none'};
             $list.css(hideElem);
 
             function toggleList(e) {
+                var listHeight = $list.height();
+                console.log(listHeight);
                 if (!$link.hasClass("z-active")) {
                     $link.addClass("z-active");
-                    TweenMax.to($list, settings.time, {height: listHeight, display: 'block', ease: Elastic.easeOut});
+                    TweenMax.fromTo($list, settings.time, {height: 0}, {height: listHeight, display: 'block', ease: Elastic.easeOut, onComplete: function () {
+                        $list.css({height: 'auto'})
+                    }});
+
                 } else {
                     $link.removeClass("z-active");
-                    TweenMax.to($list, settings.time, {height: 0, ease: Elastic.easeOut});
+                    TweenMax.to($list, settings.time, {height: 0, ease: Elastic.easeOut, onComplete: function () {
+                        $list.css({height: 'auto', display: 'none'});
+                    }});
                 }
                 e.preventDefault();
             }
-
             $($link).click(toggleList);
         })
     }
