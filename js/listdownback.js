2e04ed4ac36c9d5aedfd552085d9f067b0f6d46d
nailu0 <strivek@163.com>
2014-05-16 21:47:34 +0800

    增加了许多项目中遇到的问题

--- js/listdownback.js
+++ js/listdownback.js
@@ -6,30 +6,39 @@ $((function () {
         //默认时间
         var settings = $.extend({time: 0.6}, options);
         return $.each(this, function (options) {
-            var $list = $(this).find(" > ul"),
-                $link = $(this).find("> :first-child"),
-                $linkParent = $link.parent();
+//            var $list = $(this).find(" > ul"),
+//                $link = $(this).find("> :first-child"),
+//                $linkParent = $link.parent();
+
+            var $list = $(this).find("ul"),
                 hideElem = {overflow: 'hidden', display: 'none'};
+
             $list.css(hideElem);
-
             function toggleList(e) {
-                var listHeight = $list.height();
-                console.log(listHeight);
-                if (!$linkParent.hasClass("z-active")) {
-                    $linkParent.addClass("z-active");
-                    TweenMax.fromTo($list, settings.time, {height: 0}, {height: listHeight, display: 'block', ease: Elastic.easeOut, onComplete: function () {
-                        $list.css({height: 'auto'})
-                    }});
 
-                } else {
-                    $linkParent.removeClass("z-active");
-                    TweenMax.to($list, settings.time, {height: 0, ease: Elastic.easeOut, onComplete: function () {
-                        $list.css({height: 'auto', display: 'none'});
-                    }});
-                }
-                e.preventDefault();
             }
-            $($link).click(toggleList);
+
+//
+//            function toggleList(e) {
+//                var listHeight = $list.height();
+//                console.log(listHeight);
+//                if (!$linkParent.hasClass("z-active")) {
+//                    $linkParent.addClass("z-active");
+//                    TweenMax.fromTo($list, settings.time, {height: 0}, {height: listHeight, display: 'block', ease: Elastic.easeOut, onComplete: function () {
+//                        $list.css({height: 'auto'})
+//                    }});
+//
+//                } else {
+//                    $linkParent.removeClass("z-active");
+//                    TweenMax.to($list, settings.time, {height: 0, ease: Elastic.easeOut, onComplete: function () {
+//                        $list.css({height: 'auto', display: 'none'});
+//                    }});
+//                }
+//                e.preventDefault();
+//            }
+//            $($link).click(toggleList);
+            var list = $(this)
+
         })
     }
 })());
